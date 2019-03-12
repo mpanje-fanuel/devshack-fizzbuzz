5130b938aa6a3708b4f6a6182d12fca165e1b5f9
Fanuel Mpanje <fmpanje@zss.co.zw>
2019-03-12 11:54:43 +0200

    Optimize print numbers program

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        String left = "devshack";
-        String right = "devshackIntl";
-        System.out.println("============== CONVERT "+left+" into "+right+" ==============");
-        System.out.println();
-        PrintNumbers.printNumbers(9, new ArrayList<>(Arrays.asList(38, 95, 29, 22, 15, 49, 34, 70, 93)));
-        System.out.println();
-        System.out.println("============== CONVERSION COMPLETE!==============");
-
+        PrintNumbers.printNumbers(9, "38, 95, 29, 22, 15, 49, 34, 70, 93");
     }
 }

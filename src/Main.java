d64a05ccd8e19c4074d661369fe340850b8859e2
Fanuel Mpanje <fmpanje@zss.co.zw>
2019-03-12 12:02:45 +0200

    space separated N inout

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PrintNumbers.printNumbers(9, "38, 95, 29, 22, 15, 49, 34, 70, 93");
+        PrintNumbers.printNumbers(9, "38 95 29 22 15 49 34 70 93");
     }
 }
